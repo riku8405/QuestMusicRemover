fff2a7c27fbefdd5965748998bcc2fc2c1592c32
riku8405 <70416702+riku8405@users.noreply.github.com>
2023-08-07 18:42:54 -0400

    feat: Add configuration

--- Patcher.cs
+++ Patcher.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Aki.Reflection.Patching;
+using BepInEx.Configuration;
 using Comfort.Common;
 using EFT.UI;
 using UnityEngine;
@@ -9,31 +12,45 @@ namespace QuestMusicRemover
 {
     internal class GUISoundsPatch
     {
-        static EUISoundType[] bannedSoundTypes = {
-            EUISoundType.QuestCompleted,
-            EUISoundType.QuestFailed,
-            EUISoundType.QuestFinished,
-            EUISoundType.QuestStarted,
-            EUISoundType.QuestSubTrackComplete
-        };
-
         public class PatcherClass : ModulePatch
         {
+            // List of EUISoundTypes that contain music
+            static EUISoundType[] bannedSoundTypes = {
+                EUISoundType.QuestStarted,
+                EUISoundType.QuestFailed,
+                EUISoundType.QuestSubTrackComplete,
+                EUISoundType.QuestFinished,
+                EUISoundType.QuestCompleted
+            };
+
+            // Dictionary of EUISoundType to the config entry
+            static Dictionary<EUISoundType, ConfigEntry<bool>> soundTypeConfigDictionary = new Dictionary<EUISoundType, ConfigEntry<bool>>()
+            {
+                { EUISoundType.QuestStarted, Plugin.disableStartMusic },
+                { EUISoundType.QuestFailed, Plugin.disableFailMusic },
+                { EUISoundType.QuestSubTrackComplete, Plugin.disableConditionCompleteMusic },
+                { EUISoundType.QuestCompleted, Plugin.disableAvailableForFinishMusic },
+                { EUISoundType.QuestFinished, Plugin.disableTurnInMusic }
+            };
+
+            // Find the Original method
             protected override MethodBase GetTargetMethod()
             {
                 return typeof(GUISounds).GetMethod("PlayUISound", BindingFlags.Instance | BindingFlags.Public);
             }
 
+            // Create a prefix patch hooking into the original method (when an EUISoundType is played)
             [PatchPrefix]
             static bool PrefixPatch(EUISoundType soundType)
             {
-                if (bannedSoundTypes.Contains(soundType))
-                {
-                    return false;
+                // If the EUISoundType is apart of the banned EUISoundTypes (that contain music)
+                if (bannedSoundTypes.Contains(soundType) && soundTypeConfigDictionary[soundType].Value == true) {
+                    return false; // Don't play the EUISoundType
                 }
+                // Otherwise
                 else
                 {
-                    return true;
+                    return true; // Play the EUISoundType
                 }
             }
         }
--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,35 @@
 using BepInEx;
-using UnityEngine;
-using EFT;
+using BepInEx.Configuration;
 
 namespace QuestMusicRemover
 {
     [BepInPlugin("com.riku8405.questmusicremover", "QuestMusicRemover", "1.0.0")]
-    [BepInDependency("com.spt-aki.core", "3.6.0")]
+    [BepInDependency("com.spt-aki.core", "3.6.1")]
     public class Plugin : BaseUnityPlugin
     {
+        // Configuration
+        public static ConfigEntry<bool> disableStartMusic { get; set; } // Disable quest start music
+        public static ConfigEntry<bool> disableFailMusic { get; set; } // Disable quest fail music
+        public static ConfigEntry<bool> disableConditionCompleteMusic { get; set; } // Disable quest condition hand over music
+        public static ConfigEntry<bool> disableAvailableForFinishMusic { get; set; } // Disable available for finish music
+        public static ConfigEntry<bool> disableTurnInMusic { get; set; } // Disable turn in music
+
         void Awake()
         {
-            Logger.LogInfo("Loaded version 1.0.0");
+            Logger.LogInfo("Loaded version 1.0.0rc2");
+        }
+
+        void Start()
+        {
+            // Set default configuration values
+            disableStartMusic = Config.Bind("Quest Music Remover", "Disable Start Music", true, "Disables music when starting a quest");
+            disableFailMusic = Config.Bind("Quest Music Remover", "Disable Fail Music", true, "Disables music when failing a quest");
+            disableConditionCompleteMusic = Config.Bind("Quest Music Remover", "Disable Condition Turn In Music", true, "Disables music when  turning in an item for a condition or a condition being completed");
+            disableAvailableForFinishMusic = Config.Bind("Quest Music Remover", "Disable Available for Finish Music", true, "Disables music when a quest is available to be completed or turned in");
+            disableTurnInMusic = Config.Bind("Quest Music Remover", "Disable Turn In Music", true, "Disables music when a quest is turned in or completed");
+
+            // Enable the Patcher
             new GUISoundsPatch.PatcherClass().Enable();
-        }    
+        }
     }
 }
